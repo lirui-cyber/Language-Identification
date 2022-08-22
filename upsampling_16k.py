dba0b62ac06826aa2f94c24e755d6b7340b8dac9
lirui-cyber <lruicor@163.com>
2022-08-22 20:14:32 +0800

    add

--- upsampling_16k.py
+++ upsampling_16k.py
@@ -8,18 +8,18 @@ def upsampling_lre(wav_scp, save_wav, save_16k):
     with open(wav_scp, 'r') as f:
         lines_wav = f.readlines()
     audio_path = [x.split()[-2].strip() for x in lines_wav]
- 
-    for audio in audio_path:
+    audio_name = [ x.split()[0].strip() for x in lines_wav] 
+    for audio in range(len(audio_path)):
         # print(audio)
-        if audio.endswith('.sph'):
-            new_name = save_wav + '/' + os.path.split(audio)[-1].replace('.sph', '.wav')
-            subprocess.call("sph2pipe -p " + audio + " " + new_name, shell=True)
+        if audio_path[audio].endswith('.sph'):
+            new_name = save_wav + '/' + audio_name[audio] + ".wav"
+            subprocess.call("sph2pipe -p " + audio_path[audio] + " " + new_name, shell=True)
             new_name_16k = new_name.replace(save_wav, save_16k)
             subprocess.call("sox " + new_name + " -r 16000 " + new_name_16k, shell=True)
             os.remove(new_name)
-        elif audio.endswith('.flac'):
-            new_name = save_16k + '/' + os.path.split(audio)[-1].replace('.flac', '.wav')
-            subprocess.call("sox " + audio + " -r 16000" + new_name, shell=True)
+        elif audio_path[audio].endswith('.flac'):
+            new_name = save_16k + '/' + audio_name[audio] + ".wav"
+            subprocess.call("sox " + audio_path[audio] + " -r 16000" + new_name, shell=True)
     os.rmdir(save_wav)
 
 wav_scp = sys.argv[1]
